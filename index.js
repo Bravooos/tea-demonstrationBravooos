de59196a7ea5e013733cdf8632ec07de9b969b48
Bravooos <162330626+Bravooos@users.noreply.github.com>
2024-03-27 10:33:11 +0000

    Added helper function

--- index.js
+++ index.js
@@ -1,56 +1,24 @@
 // Helper function to calculate the square of a number
-
-functio
 function calculateSquare(number) {
-  
- 
-return number * number;
+  return number * number;
 }
 
-
-}
-
-/
 // Helper function to check if a number is even
-
-fun
 function isEven(number) {
-  
- 
-return number % 2 === 0;
+  return number % 2 === 0;
 }
 
 // Main function to demonstrate the helper functions
-
-functi
 function main() {
-  
-  le
-let num = 5;
-  
-  conso
-console.log(`The square of ${num} is ${calculateSquare(num)}`);
+  let num = 5;
+  console.log(`The square of ${num} is ${calculateSquare(num)}`);
 
-  
-
-if (isEven(num)) {
-    
-    consol
-
-  
-console.log(`${num} is an even number.`);
-  } 
-  } els
-else {
-    
-    c
-console.log(`${num} is an odd number.`);
+  if (isEven(num)) {
+    console.log(`${num} is an even number.`);
+  } else {
+    console.log(`${num} is an odd number.`);
   }
 }
 
-
-  }
 // Call the main function
-
-main
 main();
