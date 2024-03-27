2effe803e97ae6c792378eccde58d25aea7c7d8b
Bravooos <162330626+Bravooos@users.noreply.github.com>
2024-03-27 09:44:41 +0000

    Added helper function

---
+++ index.js
@@ -0,0 +1,77 @@
+// Helper function to calculate the square of a number
+
+funct
+
+fu
+function square(number) {
+  
+  ret
+
+  
+return number * number;
+}
+
+
+}
+
+/
+
+}
+// Helper function to check if a number is even
+
+funct
+function isEven(number) {
+  
+  retu
+return number % 2 === 0;
+}
+
+
+}
+
+/
+// Helper function to find the maximum of two numbers
+
+func
+function findMax(a, b) {
+  
+  ret
+return Math.max(a, b);
+}
+
+
+}
+// Example usage of the helper functions
+
+le
+let num = 5;
+
+c
+console.log(`The square of ${num} is ${square(num)}`);
+
+
+
+i
+if (isEven(num)) {
+  
+  conso
+
+ 
+console.log(`${num} is an even number.`);
+} else {
+  
+  con
+console.log(`${num} is an odd number.`);
+}
+
+
+}
+
+le
+let x = 10;
+
+l
+let y = 20;
+
+cons
+console.log(`The maximum of ${x} and ${y} is ${findMax(x, y)}`);
